64bf5d8a750fa96aa6a41d1aa58b454050799058
GopalaPonnada <ponnada.gopala@gmail.com>
2025-02-21 14:51:05 -0500

    Essence Testing and 2D

---
+++ scripts/essence.gd
@@ -0,0 +1,10 @@
+extends Node3D
+
+@onready var outer_sphere: MeshInstance3D = $"Outer Sphere"
+@onready var inner_sphere: MeshInstance3D = $"Inner Sphere"
+
+func _process(_delta: float) -> void:
+	
+	
+	
+	pass
