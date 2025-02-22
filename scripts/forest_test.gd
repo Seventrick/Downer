971bf60c8a9418c0179168b6bb490ec483756950
Seventrick <maxseventrick@gmail.com>
2025-02-21 22:17:56 -0500

    added some basic functionality for making the level

--- scripts/forest_test.gd
+++ scripts/forest_test.gd
@@ -9,11 +9,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_area_3d_area_entered(area: Area3D) -> void:
-	pass # Replace with function body.
-
-
-func _on_area_3d_area_exited(area: Area3D) -> void:
-	pass # Replace with function body.
--- scripts/plush.gd
+++ scripts/plush.gd
@@ -1,3 +1,9 @@
 extends StaticBody3D
 
 @export var item_data: ItemData
+
+@onready var omni_light_3d: OmniLight3D = $"../OmniLight3D"
+
+func interact():
+	
+	omni_light_3d.show()
