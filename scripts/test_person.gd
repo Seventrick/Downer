7c4a400dd575a613f0406f9e056d8cf65ac36933
Seventrick <maxseventrick@gmail.com>
2025-04-13 22:04:23 -0400

    character interaction test

---
+++ scripts/test_person.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+var style: DialogicStyle = load("res://dialogic/styles/dayStyleCustom.tres")
+var cooldown = false
+@onready var timer: Timer = $Timer
+
+var entered = false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	style.prepare()
+	Dialogic.signal_event.connect(_dialogicTimer)
+
+func _dialogicTimer(argument:String):
+	if argument == "dialogicTimer":
+		timer.start()
+		cooldown = true
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if entered:
+		if Input.is_action_just_pressed("interact"):
+			if Dialogic.current_timeline == null and !cooldown:
+				Dialogic.start("dialogueTestTimeline")
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		entered = true
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		entered = false
+
+
+func _on_timer_timeout() -> void:
+	cooldown = false
